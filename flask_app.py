cca2b11ada5f1955aca886964be785f7a1fed6cb
serg988 <42653178+serg988@users.noreply.github.com>
2023-12-03 01:44:49 +0300

    added delete product fn and login_required decorator

--- flask_app.py
+++ flask_app.py
@@ -1,61 +1,101 @@
 # python3 -m venv venv
 # source venv/bin/activate
 
-from flask import Flask, render_template, flash, session, request, redirect
+from flask import Flask, render_template, flash, session, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
-from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-# DB Config
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'shop'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="root",
+    password="password",
+    hostname="localhost",
+    databasename="shop",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    email = db.Column(db.String(30))
+    password = db.Column(db.String(200))
+
+    # def __repr__(self):
+    #     return [self.name, self.text]
+
+
+class Product(db.Model):
+    __tablename__ = "product"
+    product_id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(50))
+    product_name = db.Column(db.String(20))
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    image = db.Column(db.String(500))
+
+    # def __repr__(self):
+    #     return [self.name, self.text]
+
 
 app.config['SECRET_KEY'] = os.urandom(24)
 
 
+def is_authenticated():
+    username = session.get('username')
+    return username and len(username.strip()) > 0
+
+
+def login_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not is_authenticated():
+            return redirect('login')
+        return f(*args, **kwargs)
+    return wrapper
+
+
 @app.route('/')
+@login_required
 def index():
-    cursor = mysql.connection.cursor()
-    result_value = cursor.execute("SELECT * FROM product")
-    if result_value > 0:
-        products = cursor.fetchall()
-        cursor.close()
-        return render_template('products.html', products=products)
-    return render_template('products.html')
+    products = Product.query.all()
+    return render_template('products.html', products=products)
 
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        user_details = request.form
-        username = user_details['username']
-        cursor = mysql.connection.cursor()
-        result_value = cursor.execute(
-            "SELECT * FROM user WHERE username = %s", ([username]))
-        if result_value > 0:
-            user = cursor.fetchone()
-            if check_password_hash(user['password'], user_details['password']):
-                session['login'] = True
-                if username.lower() == 'admin':
-                    session['is_admin'] = True
-
-                return redirect('/')
-            else:
-                cursor.close()
-                flash("Invalid credentials!", 'danger')
-                return render_template('/login.html')
+    if request.method == "POST":
+        if not request.form["username"] or not request.form["password"]:
+            flash("Поля не должны быть пустыми.", 'danger')
+            return render_template('login.html')
         else:
-            cursor.close()
-            flash(f"User '{user_details['username']}' not found!", 'danger')
-            return render_template('/login.html')
+            entered_username = request.form["username"]
+            password = request.form["password"]
+            user_stored = User.query.filter_by(
+                username=entered_username).first()
+            if not user_stored:
+                flash("Неправильное имя пользователя или пароль", 'danger')
+                return redirect('/login')
+            if not check_password_hash(user_stored.password, password):
+                flash('Неправильное имя пользователя или пароль', 'danger')
+                return render_template('/login.html')
+            session['username'] = entered_username
+            session['login'] = True
+            if entered_username.lower() == 'admin':
+                session['is_admin'] = True
+            return redirect('/')
     return render_template('login.html')
 
 
@@ -64,52 +104,46 @@ def register():
     if request.method == 'POST':
         user_details = request.form
         if user_details['password'] != user_details['confirmPassword']:
-            flash("Passwords do not match! Try again.", 'danger')
+            flash("Пароли не совпадают. Попробуйте еще.", 'danger')
             return render_template('register.html')
-        cursor = mysql.connection.cursor()
-        cursor.execute(
-            "INSERT INTO user(first_name, last_name, username, email, password) VALUES (%s, %s, %s, %s, %s)",
-            (
-                user_details["firstname"],
-                user_details["lastname"],
-                user_details["username"],
-                user_details["email"],
-                generate_password_hash(user_details["password"]),
-            ),
-        )
-
-        mysql.connection.commit()
-        cursor.close()
-        flash("Registration successful! Please login.", 'success')
-        return redirect('/login')
+        else:
+            user = User(
+                username=request.form["username"],
+                first_name=request.form["firstname"],
+                last_name=request.form["lastname"],
+                email=request.form["email"],
+                password=generate_password_hash(request.form["password"]),
+            )
+            db.session.add(user)
+            db.session.commit()
+            flash("Registration successful! Please login.", 'success')
+            return redirect('/login')
     return render_template('register.html')
 
 
 @app.route('/product-form', methods=['GET', 'POST'])
 def product_form():
-    print('Session', session)
-    if request.method == 'POST':
-        product_details = request.form
-        cursor = mysql.connection.cursor()
-        cursor.execute(
-            "INSERT INTO product(category, product_name, quantity, price, image) VALUES (%s, %s, %s, %s, %s)",
-            (
-                product_details["category"],
-                product_details["product_name"],
-                product_details["quantity"],
-                product_details["price"],
-                product_details["image"]
-            ),
+    if request.method == 'POST' and request.form.get('del'):
+        Product.query.filter(Product.product_id == request.form['del'][6:]).delete()
+        db.session.commit()
+        return redirect('/')
+    elif request.method == 'POST':
+        product = Product(
+            category=request.form["category"],
+            product_name=request.form["product_name"],
+            quantity=request.form["quantity"],
+            price=request.form["price"],
+            image=request.form["image"],
         )
-
-        mysql.connection.commit()
-        cursor.close()
+        db.session.add(product)
+        db.session.commit()
         flash("Товар добавлен!", 'success')
-        # return redirect('/product-form')
+        return redirect('/product-form')
     return render_template('product-form.html')
 
 
 @app.route('/about')
+@login_required
 def about():
     return render_template('/about.html')
 
